5e82d206b085bf3163c35baa97749b3b37f5a8ad
nikita <ntarasov29@gmail.com>
2026-02-25 23:57:33 +0300

    Add Bluetooth permission request button in MainActivity

--- app/src/main/java/org/google/android/marvin/brailleback/MainActivity.java
+++ app/src/main/java/org/google/android/marvin/brailleback/MainActivity.java
@@ -1,12 +1,32 @@
 package org.google.android.marvin.brailleback;
 
+import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        
+        Button btn = new Button(this);
+        btn.setText("РАЗРЕШИТЬ BLUETOOTH И ПОДКЛЮЧИТЬ");
+        btn.setOnClickListener(v -> requestPermissions(new String[]{
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN
+        }, 1));
+        
+        layout.addView(btn);
+        setContentView(layout);
     }
 }
