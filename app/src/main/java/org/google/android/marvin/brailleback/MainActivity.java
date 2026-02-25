8e40ef09238197bc0363c2676cb2ca0890f899d7
nikita <ntarasov29@gmail.com>
2026-02-25 22:55:54 +0300

    Update MainActivity to use activity_main layout

--- app/src/main/java/org/google/android/marvin/brailleback/MainActivity.java
+++ app/src/main/java/org/google/android/marvin/brailleback/MainActivity.java
@@ -2,14 +2,11 @@ package org.google.android.marvin.brailleback;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("BrailleBack Modern активен. Настройте его в Спец. возможностях.");
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
